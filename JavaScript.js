29f61a6b5a6f6aa03ff8f6dc41a90e8fea56f2b0
nhatsimmy <102749129+nhatsimmy@users.noreply.github.com>
2022-03-31 23:12:56 +0700

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,38 @@
+							function nenden() {
+																document.getElementById("nen").style.backgroundColor="DarkSlateGrey";
+																document.getElementById("nen").style.color=" Magenta";
+																}
+																function nentrang() {
+																document.getElementById("nen").style.backgroundColor="white";
+																document.getElementById("nen").style.color="black";
+																}
+																function nentim() {
+																 document.getElementById("nen").style.backgroundColor="violet"; 
+																 	 }
+																 	 function nendo() {
+																 	 				 document.getElementById("nen").style.backgroundColor="red";
+																 	 }
+																 	 function nenvang() {
+																 	 document.getElementById("nen").style.backgroundColor="gold";
+																 	 }
+																 	 function nenxanhla() {
+																 	 document.getElementById("nen").style.backgroundColor="green";}
+																 	 function nenxanhdatroi() {
+																 	 document.getElementById("nen").style.backgroundColor="blue";
+																 	 }
+																 	 function nendendam() {
+																 	 document.getElementById("nen").style.backgroundColor="black";
+																 	 document.getElementById("nen").style.color="white";
+																 	 }
+																 	 function nenhong() {
+																 	 document.getElementById("nen").style.backgroundColor="pink";
+																 	 }
+																 	 function nenxanhlanhat() {
+																 	 document.getElementById("nen").style.backgroundColor="lightgreen";
+																 	 	}
+																 	 	function nenxanhdatroinhat() {
+																 	 	 document.getElementById("nen").style.backgroundColor="lightblue";
+																 	 	 }
+																 	 	 function nencam() {
+																 	 	 document.getElementById("nen").style.backgroundColor=" Orange";
+																 	 	 }
